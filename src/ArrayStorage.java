7228ff10b6f39d8bc2c22a7db6d66e60e6c96864
serzh <serzh.beastforever.alekhin@inbox.ru>
2021-01-19 22:34:17 +0700

    homework 1.4 Готовое

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10];
+    Resume[] storage = new Resume[10000];
     int size = 0;
-    int index=0;
 
     void clear() {
         Arrays.fill(storage, null);
-        size=0;
+        size = 0;
     }
 
     void save(Resume r) {
@@ -19,14 +18,26 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
-        index = Integer.parseInt(uuid);
-        for (int i = index; i < size; i++) {
-            storage[i] = storage[i + 1];
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                index = i;
+            }
+        }
+        if (index > -1) {
+            for (int i = index; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[size - 1] = null;
         }
         size--;
     }
@@ -35,8 +46,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return storage;
-
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
